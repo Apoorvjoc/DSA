53b13588037a3319663e4083ed03bf402a43f274
Apoorvjoc <hlwapoorv@gmail.com>
2025-04-22 01:27:31 +0530

    approach 2 (nodes at dist k) to find burn tree

--- src/Trees/BurnTree.java
+++ src/Trees/BurnTree.java
@@ -4,59 +4,140 @@ import java.util.*;
 
 public class BurnTree {
     public int amountOfTime(TreeNode root, int start) {
-        Map<Integer , List<Integer>> parent = new HashMap();
-        makeGraph(root , -1 , parent);
+        int time = 0;
+        if(root == null)return time;
+
+        Map<Integer, List<Integer> > graph = new HashMap();
+        graph.put(root.val , new ArrayList());
+        constructGraphFromTree(root , graph);
+
+        // apply bfs for graph:
+        System.out.print(graph);
 
         Queue<Integer> q = new ArrayDeque();
         q.add(start);
         Set<Integer> vis = new HashSet();
         vis.add(start);
 
-        int time = 0;
-
         while(!q.isEmpty()){
             int n = q.size();
 
-            for(int i = 0; i < n ; i++){
-                Integer front = q.poll();
-                List<Integer> li = parent.get(front);
+            for(int i = 0 ; i<n ; i++){
+                int front = q.poll();
+                List<Integer> list = graph.get(front);
 
-                for(int x : li){
-                    if(!vis.contains(x)){
-                        vis.add(x);
-                        q.add(x);
+                for(int ele : list){
+                    if(!vis.contains(ele)){
+                        q.add(ele);
+                        vis.add(ele);
                     }
                 }
             }
             time++;
         }
 
-        return time-1;
+        return time - 1;
     }
 
-    private void makeGraph(TreeNode curr , int val , Map<Integer , List<Integer>> adj){
-        if(curr == null)return;
+    // if all vales of tree node are not same then we have to follow this
+    private void constructGraphFromTree(TreeNode root , Map<Integer, List<Integer> > graph){// making graph from tree
+        if(root == null)return;
 
-        adj.putIfAbsent(curr.val , new ArrayList<>());
-
-        //adding parent
-        if(val != -1){
-            adj.get(curr.val).add(val);
+        if(root.left != null){
+            graph.putIfAbsent(root.left.val, new ArrayList<>());
+            graph.get(root.val).add(root.left.val);
+            graph.get(root.left.val).add(root.val); // making graph bi-directional
         }
 
-        if(curr.left != null){
-            adj.putIfAbsent(curr.left.val, new ArrayList<>());
-            adj.get(curr.val).add(curr.left.val);
-            adj.get(curr.left.val).add(curr.val);  // Add back the parent (bidirectional edge)
+        if(root.right != null){
+            graph.putIfAbsent(root.right.val, new ArrayList<>());
+            graph.get(root.val).add(root.right.val);
+            graph.get(root.right.val).add(root.val); // making graph bi-directional
         }
 
-        if(curr.right != null){
-            adj.putIfAbsent(curr.right.val, new ArrayList<>());
-            adj.get(curr.val).add(curr.right.val);
-            adj.get(curr.right.val).add(curr.val);  // Add back the parent (bidirectional edge)
+        constructGraphFromTree(root.left , graph);
+        constructGraphFromTree(root.right , graph);
+
+    }
+
+}
+
+
+//approach 2 extension of burn tree
+class burnTreeExtended{
+    private TreeNode findNodeInTree(int start , TreeNode root){
+        if(root == null)return null;
+
+        if(root.val ==  start)return root;
+
+        TreeNode left = findNodeInTree(start , root.left);
+        TreeNode right = findNodeInTree(start , root.right);
+
+        if(left != null)return left;
+        if(right != null) return right;
+
+        return null;
+    }
+
+    public int amountOfTime(TreeNode root, int start) {
+        int ans = 0;
+        if(root == null)return ans;
+
+        TreeNode startNode = findNodeInTree(start , root); // as start is given in int therefore finding its address , as given in question all nodes are unique
+
+        System.out.println(startNode.val);
+
+        Map<TreeNode , TreeNode> mp = new HashMap();
+        findParent(mp , root);
+
+        Queue<TreeNode>q = new ArrayDeque();
+        q.add(startNode);
+
+        Set<TreeNode> vis = new HashSet();
+        vis.add(startNode);
+
+        while(!q.isEmpty()){
+            int n = q.size();
+
+            for(int i = 0 ; i<n ; i++){
+                TreeNode front = q.poll();
+
+                if(front.left != null && !vis.contains(front.left)){
+                    q.add(front.left);
+                    vis.add(front.left);
+                }
+
+                if(front.right != null && !vis.contains(front.right)){
+                    q.add(front.right);
+                    vis.add(front.right);
+                }
+
+                if(mp.containsKey(front)){
+                    TreeNode parent = mp.get(front);
+                    if(!vis.contains(parent)){
+                        q.add(parent);
+                        vis.add(parent);
+                    }
+                }
+            }
+
+            ans++;
         }
 
-        makeGraph(curr.left , curr.val , adj);
-        makeGraph(curr.right , curr.val , adj);
+        return ans-1;
+    }
+
+    private void findParent(Map<TreeNode , TreeNode> mp , TreeNode root){
+
+        if(root == null)return;
+
+        if(root.left != null)mp.put(root.left , root);
+
+        if(root.right != null)mp.put(root.right , root);
+
+        findParent(mp , root.left);
+        findParent(mp , root.right);
+
+
     }
 }
